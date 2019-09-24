c5426d652349b3955701b4a35aca65d3b1120db4
Llynch1998 <llynch1998@gmail.com>
2019-09-24 01:56:46 -0400

    fixed html css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 let rows = 12;
 let columns = 16;
 let attacks = [];
-let punch = new Attack("Punch","Physical",100,0,5);
-attacks.push(punch);
+
 let size = 40;
 let tiles = []; // array for tiles
 let party = [];
@@ -12,49 +11,49 @@ let goblinImg,kingSlimeImg,chickenImg,skelboiImg,slimeImg;
 let downImg,rightImg,leftImg,upImg;
 
 //hp,atk,def,spd,mp,sprites,name,lvl,abilities
-let goblin = {
-    hp:20,
-    atk:5,
-    def:5,
-    spd:10,
-    mp:0,
-    sprites:goblinImg,
-    name:"Goblin",
-    lvl:1,
-    abilities = [new Attack("Punch","Physical",20,0,5),
-    new Attack("Poke","Physical",5,0,15),
-    new Attack("Chomp","Physical",10,0,10)]
-    //name, type, uses, cost, power
-}
+// let goblin = {
+//     hp:20,
+//     atk:5,
+//     def:5,
+//     spd:10,
+//     mp:0,
+//     sprites:goblinImg,
+//     name:"Goblin",
+//     lvl:1,
+//     abilities = [punch = new Attack("Punch","Physical",20,0,5),
+//     poke = new Attack("Poke","Physical",5,0,15),
+//     new Attack("Chomp","Physical",10,0,10)]
+//     //name, type, uses, cost, power
+// }
 
-let Skeleton = {
-    hp:10,
-    atk:15,
-    def:0,
-    spd:20,
-    mp:0,
-    sprites:skelboiImg,
-    name:"Skeleton",
-    lvl:1,
-    abilities = [new Attack("Slam","Physical",20,0,5),
-    new Attack("Stab","Physical",5,0,15)]
-    //name, type, uses, cost, power
-}
+// let Skeleton = {
+//     hp:10,
+//     atk:15,
+//     def:0,
+//     spd:20,
+//     mp:0,
+//     sprites:skelboiImg,
+//     name:"Skeleton",
+//     lvl:1,
+//     abilities = [new Attack("Slam","Physical",20,0,5),
+//     new Attack("Stab","Physical",5,0,15)]
+//     //name, type, uses, cost, power
+// }
 
-let Chicken = {
-    hp:30,
-    atk:5,
-    def:10,
-    spd:10,
-    mp:0,
-    sprites:chickenImg,
-    name:"Chicken",
-    lvl:1,
-    abilities = [new Attack("Peck","Physical",20,0,10),
-    new Attack("Neck Whip","Physical",5,0,15),
-    new Attack("Tail Slap","Physical",10,0,20)]
-    //name, type, uses, cost, power
-}
+// let Chicken = {
+//     hp:30,
+//     atk:5,
+//     def:10,
+//     spd:10,
+//     mp:0,
+//     sprites:chickenImg,
+//     name:"Chicken",
+//     lvl:1,
+//     abilities = [new Attack("Peck","Physical",20,0,10),
+//     new Attack("Neck Whip","Physical",5,0,15),
+//     new Attack("Tail Slap","Physical",10,0,20)]
+//     //name, type, uses, cost, power
+// }
 
 function preload(){//loads images for use later
     //enemies
